90307d3b6b079a926233fbe3d19380ba8357f25b
edgarNet <nefertumdark@gmail.com>
2017-01-21 18:08:32 -0600

    Multithreading

--- SWK/SWK/Program.cs
+++ SWK/SWK/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace SWK
 {
@@ -9,8 +10,20 @@ namespace SWK
     {
         static void Main(string[] args)
         {
+            var oAlpha = new ThreadGame();
+            Thread oThread = new Thread(new ThreadStart(oAlpha.Ejecuta));
+            oThread.Start();
+        }
+    }
+
+    public class ThreadGame
+    {
+        // This method that will be called when the thread is started
+        public void Ejecuta()
+        {
             var push = new Pusheador();
             push.Proceso();
         }
     }
+
 }
